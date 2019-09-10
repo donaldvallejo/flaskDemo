a2b0d78841af432494ca0d0701b808b432f0e614
Donald Vallejo <donny.vallejo@gmail.com>
2019-09-10 11:33:21 -0700

    fixed jinja template loop. about to work on my name to appear

--- app.py
+++ app.py
@@ -16,8 +16,14 @@ def index():
 def get_compliment():
     """Give the user a compliment"""
     name = request.args.get('name')
-    show_compliments = request.args.get('show_compliments')
-    compliments_to_show = sample(compliments, 3)
+    show_compliments = request.args.get('show_compliments') == 'on'
+    print('show_compliments:', show_compliments)
+    num_compliments = int(request.args.get('num_compliments'))
+    debug_str = ' '.join(map(str, ['num_compliments:', num_compliments, 'type:', type(num_compliments)]))
+    print(debug_str)
+    # return debug_str
+    compliments_to_show = sample(compliments, num_compliments)
+    print(compliments_to_show)
 
     return render_template(
         'compliments.html', 
